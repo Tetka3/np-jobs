a1177ccfea000abdc9d4588926bf7107f3b9d5f0
Titus Njema <njema3@gmail.com>
2024-10-31 16:27:12 +0300

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,17 @@ import './style.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      Footer
+      <div className="leftFooter">
+        <p>Information</p>
+        <ul>
+          <li>About JobSeek</li>
+          <li>Contact</li>
+          <li>Notices</li>
+          <li>Careers</li>
+        </ul>
+      </div>
+      <div className="middleFooter"></div>
+      <div className="rightFooter"></div>
     </div>
   )
 }
