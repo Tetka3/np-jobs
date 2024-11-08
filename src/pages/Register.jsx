9ba7bcf5ee856aea496fa6328151684429fd673b
Titus Njema <njema3@gmail.com>
2024-11-08 15:50:19 +0300

    first commit

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,17 +11,19 @@ const Register = () => {
       <div className='jobsImage'>
         <img src={hire}/>
       </div>
-      <form>
-        <label>Username</label>
-        <input type='text' placeholder='Enter Username...'/>
-        <label>Email</label>
-        <input type='text' placeholder='Enter Your Email...'/>
-        <label>Password</label>
-        <input type='password' />
-        <label>Repeat Password</label>
-        <input type='password' />
-        <button onClick={() => navigate('/np-jobs')}>Register</button>
-      </form>
+      <div className="registerForm">
+        <form>
+          <label>Username</label>
+          <input type='text' placeholder='Enter Username...'/>
+          <label>Email</label>
+          <input type='text' placeholder='Enter Your Email...'/>
+          <label>Password</label>
+          <input type='password' />
+          <label>Repeat Password</label>
+          <input type='password' />
+          <button onClick={() => navigate('/np-jobs')}>Register</button>
+        </form>
+      </div>      
     </div>
   )
 }
