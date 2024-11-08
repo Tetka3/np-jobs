05ea17068a52ccd72f4f0638a25e4cb2f4176cfb
Titus Njema <njema3@gmail.com>
2024-11-08 15:38:41 +0300

    first commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,12 +13,15 @@ const Login = () => {
       </div>
       <div className='loginForm'>
         <h3>Welcome Back!</h3>
-        <p>Not Joined JobbSeek? Register Now free of charge</p>
+        <p>Not Joined JobSeek? Register Now free of charge</p>
         <form>
           <label>Email</label>
           <input type='text' placeholder='Enter Your Email'/>
           <label>Password</label>
           <input type='password' placeholder='Enter Password here...'/>
+          <div className="checkboxInput">
+              <input type='checkbox'/><p>Remember me</p> 
+          </div>          
           <button onClick={() => navigate('/np-jobs')}>Log In</button>
       </form>
       </div>
