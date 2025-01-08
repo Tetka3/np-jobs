2492c88b6e780f0348f02144dc2fdbe369320753
Titus Njema <njema3@gmail.com>
2025-01-08 19:15:01 +0300

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,8 @@ const Sidebar = () => {
       <ul>
         <li>My Jobs</li>
         <li>Saved Jobs</li>
+        <li>Favorite Jobs</li>
+        <li>More Jobs</li>
         <li></li>
         <li></li>
         <li></li>
