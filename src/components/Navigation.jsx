f4c144141b306a830bd44a83936b406ce8072697
Titus Njema <njema3@gmail.com>
2025-01-17 17:31:06 +0300

    first commit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,7 +4,7 @@ const Navigation = () => {
   return (
     <div className='navigationBar'>
       Search By Job Category <input type='text' placeholder='Search' />
-      <input type='text' placeholder='Search' />
+      Saved Jobs <input type='text' placeholder='Search' />
       <input type='text' placeholder='Search' />
     </div>
   )
